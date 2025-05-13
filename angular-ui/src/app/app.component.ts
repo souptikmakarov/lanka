c05d76559cd1f2f9a719bfbf0a20d312fd7f513e
Souptik Banerjee <souptikb7@gmail.com>
2025-05-13 10:08:18 +0000

    implement connect, join and start game in UI

--- angular-ui/src/app/app.component.ts
+++ angular-ui/src/app/app.component.ts
@@ -8,33 +8,51 @@ import { GameDataService } from './services/game-data.service';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-ui';
   message: string = '';
   private gameStateSubscription: Subscription | undefined;
   private gameStartedSubscription: Subscription | undefined;
+  private gameResetSubscription: Subscription | undefined;
 
-  constructor(private socketService: SocketService, private gameDataService: GameDataService, private router: Router) {}
+  constructor(
+    private socketService: SocketService,
+    private gameDataService: GameDataService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.socketService.connect();
-    let playerIdSubscription = this.gameDataService.playerId.subscribe((playerId: string | null) => {
-      if (playerId) {
-        console.log("Player ID:", playerId);
-        this.router.navigate(['/lobby']);
-        playerIdSubscription?.unsubscribe();
+    let playerIdSubscription = this.gameDataService.playerId.subscribe(
+      (playerId: string | null) => {
+        if (playerId) {
+          console.log('Player ID:', playerId);
+          this.router.navigate(['/lobby']);
+          playerIdSubscription?.unsubscribe();
+        }
       }
-    });
-    this.gameStateSubscription = this.socketService.listen('game_state').subscribe((state: any) => {
-      console.log("game_state", JSON.stringify(state));
-      this.gameDataService.gameState.next(state);
-    });
-    this.gameStartedSubscription = this.socketService.listen('game_started').subscribe((state: any) => {
-      console.log("game_started", JSON.stringify(state));
-      this.router.navigate(['/game']);
-    });
+    );
+    this.gameStateSubscription = this.socketService
+      .listen('game_state')
+      .subscribe((state: any) => {
+        console.log('game_state', JSON.stringify(state));
+        this.gameDataService.gameState.next(state);
+      });
+    this.gameStartedSubscription = this.socketService
+      .listen('game_started')
+      .subscribe(() => {
+        console.log('game_started');
+        this.router.navigate(['/game']);
+      });
+    this.gameResetSubscription = this.socketService
+      .listen('game_reset')
+      .subscribe(() => {
+        console.log('game_reset');
+        this.gameDataService.gameState.next(null);
+        this.router.navigate(['/lobby']);
+      });
   }
 
   sendMessage(): void {
@@ -44,6 +62,9 @@ export class AppComponent {
   ngOnDestroy(): void {
     if (this.gameStateSubscription) {
       this.gameStateSubscription.unsubscribe();
+    }
+    if (this.gameStartedSubscription) {
+      this.gameStartedSubscription.unsubscribe();
     }
     this.socketService.disconnect();
   }
--- angular-ui/src/app/lobby/lobby.component.ts
+++ angular-ui/src/app/lobby/lobby.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { SocketService } from '../services/socket.service';
 
 @Component({
   selector: 'app-lobby',
   imports: [],
   templateUrl: './lobby.component.html',
-  styleUrl: './lobby.component.scss'
+  styleUrl: './lobby.component.scss',
 })
 export class LobbyComponent {
+  private name: string = "";
+  private team: string = "";
+  private error: string = "";
 
+  constructor(private socket: SocketService) {}
+
+  public join() {
+    this.socket.emit('join_game', { name: this.name, team: this.team });
+  }
+
+  public startGame() {
+    this.socket.emit('start_game', '');
+  }
 }
